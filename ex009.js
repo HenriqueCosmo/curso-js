641fdeb4faab0aeb79c721c572e47e99b00334c3
Henrique Cosmo <124743713+HenriqueCosmo@users.noreply.github.com>
2023-05-09 16:13:14 -0300

    treino js

---
+++ ex009.js
@@ -0,0 +1,8 @@
+var vel = 50
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+
+if(vel > 60){ //condição simples 
+    console.log('Voce ultrapassou o limite perimitdo, MULTADO!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança')
---
+++ ex010.js
@@ -0,0 +1,7 @@
+var pais = "França"
+console.log(`vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Brasileiro!')
+} else {
+    console.log('Voce é estrangeiro!')
+}
