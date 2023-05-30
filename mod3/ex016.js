20fa6fe464bf02da87fd89818ec10451a98fe072
Henrique Cosmo <124743713+HenriqueCosmo@users.noreply.github.com>
2023-05-30 12:05:46 -0300

    laços

---
+++ mod3/ex016.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+/*estrutura de repetição com laço de controle*/
+console.log('VAI COMEÇAR')
+for (var c = 1; c <= 10; c++){
+    console.log(`passo ${c}`)
+}
+console.log('FIM')
+
+
+
+
+
+
+
+
+
+
+/*
+laço de repetição com teste lógico no final
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 6)*/
+
+
+
+
+/*
+laço de repetição com teste lógico no inicio 
+var cont = 1
+while (cont < 6){
+    console.log(`passo ${cont}`)
+    cont++
+}*/
+
+/*console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')*/
+
+
