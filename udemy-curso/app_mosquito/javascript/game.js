07ca61c8edeab7a7aa577a42825fd6d58a3f214a
Henrique Cosmo <124743713+HenriqueCosmo@users.noreply.github.com>
2023-07-14 15:51:14 -0300

    adicionando funcionalidades

--- udemy-curso/app_mosquito/javascript/game.js
+++ udemy-curso/app_mosquito/javascript/game.js
@@ -1,6 +1,7 @@
 //definicao das variaveis de  altura e largura para criar a janela do jogo 
 let altura = 0;
 let largura = 0;
+let vidas = 1;
 
 //funcao para resgatar o valor da janela, altura e largura 
 function ajustaTamanho(){
@@ -14,8 +15,20 @@ function ajustaTamanho(){
 ajustaTamanho()
 
 
-
 function posicaoRandomica(){
+    /*condicao que verifica se existe algum elemento id com a tag 'mosca', caso exista ele ira remover o item, essa condicao foi usada para permitir que sejam incluidas moscas em um intervalo de tempo, e que a cada mosca incluida, a anterior seja removida antes */
+    if(document.getElementById('mosca')){
+        document.getElementById('mosca').remove()
+        //condicao para verificar se o numero de vidas retiradas  é maior que 3, enquanto nao for o usuario ainda consegue jogar 
+        if(vidas > 3){
+            alert('game over')
+        }else{
+
+            document.getElementById('life' + vidas).src = "../imagens/coracao_vazio.png"
+            vidas++ 
+        }
+    }
+
     /*cria uma posicao aleatoria para x e y, multiplcando pela altura e largura, possibilitando a criacao das moscas em diferentes areas da janela de forma aleatoria */
     let posicaoX = Math.floor( Math.random() * largura) - 90;
     let posicaoY = Math.floor( Math.random() * altura) - 90 ;
@@ -30,13 +43,40 @@ function posicaoRandomica(){
     let mosca = document.createElement('img');
     mosca.src = '../imagens/mosca.png';
     document.body.appendChild(mosca);
-    mosca.className = 'mosca1';
-
+    mosca.className = tamanhoMosca() + ' ' + ladoAleatorio();
+    mosca.id = 'mosca'
     //atribucao dos valores das posicoes para o estilo do elemento
     mosca.style.left = posicaoX + 'px';
     mosca.style.top = posicaoY + 'px';
     mosca.style.position = 'absolute';
+    //funcao apartir do metodo onclick que remove a mosca antes dela ser removida automaticamente
+    mosca.onclick = function(){
+        this.remove()
+    }
+}
+//funcao que define tres tamanhos para a mosca, atravez de atribuicao de uma classe que esta ligada ao atributo do css e com tamanhos pre definidos 
+function tamanhoMosca(){
+    let classe = Math.floor(Math.random()* 3);
 
+    switch (classe){
+        case 0:
+            return 'mosca1'
+        case 1:
+            return 'mosca2'
+        case 2:
+            return 'mosca3'
+    }
 }
 
+//funcao que define um lado aleatorio para a mosca, se for um valor 0 ela vira pra um lado, se for 1 ela vira para o outro, esta funcao tbm esta atribuindo uma classe ao css com lados configurados 
+function ladoAleatorio(){
+    classe =  Math.floor(Math.random() * 2)   
+
+    switch (classe){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
 
